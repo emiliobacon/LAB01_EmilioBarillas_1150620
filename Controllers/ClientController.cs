ec9570010368254a415d934350c4c1b830bac2dc
emiliobacon <eabarillasco@correo.url.edu.gt>
2022-08-17 12:05:18 -0600

    Client controller created

---
+++ Controllers/ClientController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Laboratorio01.Controllers
+{
+    public class ClientController
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        public string FullName { get; set; }
+
+        [Required]
+        public string Address { get; set; }
+
+        [Required]
+        public DateTime Birthday { get; set; }
+
+    }
+}
+
